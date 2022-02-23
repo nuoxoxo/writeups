4df3c600a3747575442158af2dfdb188fc7ff0db
nuo.o <49533950+nuoxoxo@users.noreply.github.com>
2022-02-23 16:11:05 +0100

    Create ftctf_force_brute.py

---
+++ scripts/ftctf_force_brute.py
@@ -0,0 +1,15 @@
+import hashlib
+
+#comp = ['free', 'iliad', '42', 'mediawan', 'scaleway']
+#name = ['delphine', 'elisa', 'michel', 'camille', 'john', 'jules']
+
+comp = ['Free', 'Iliad', '42', 'Mediawan', 'Scaleway']
+name = ['Delphine', 'Elisa', 'Michel', 'Camille', 'John', 'Jules']
+
+for i in range(43):
+    for r in range(1, 51):
+        for c in comp:
+            for n in name:
+                s = str(i) + c + str(r) + n
+                res = hashlib.md5(s.encode())
+                print(s, res.hexdigest())
